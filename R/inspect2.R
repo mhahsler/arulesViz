bff89e3ff0f9fee65f334f01069f7f6391e2dc00
mhahsler <michael@hahsler.net>
2016-09-23 19:48:15 -0500

    Added NEWS to built.

---
+++ R/inspect2.R
@@ -0,0 +1,39 @@
+#######################################################################
+# arulesViz - Visualizing Association Rules and Frequent Itemsets
+# Copyrigth (C) 2011 Michael Hahsler and Sudheer Chelluboina
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+
+# use library DT
+
+inspect2 <- function(x, ...) UseMethod("inspect2", x)
+
+inspect2.default <- function(x, ...) 
+  stop("inspect 2 not implemented for class ", class(x))
+
+inspect2.rules <- function(x, precision = 2, ...) {
+  datatable(
+    data.frame(LHS = labels(lhs(x)), RHS = labels(rhs(x)), 
+      quality(x)), filter = "top", ...) %>% formatRound(3:(3+ncol(quality(x))), 
+        precision)
+}
+
+inspect2.itemsets <- function(x, precision = 2, ...) {
+  datatable(
+    data.frame(items = labels(x), 
+      quality(x)), filter = "top", ...) %>% formatRound(2:(2+ncol(quality(x))), 
+        precision)
+}
